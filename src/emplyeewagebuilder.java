81c6d6974b17c208ac13b2fc19841381c5344e5e
sanketb412 <sanketbagde38@gmail.com>
2021-04-26 15:42:50 +0530

    Added code for Attendance Functionality

--- src/emplyeewagebuilder.java
+++ src/emplyeewagebuilder.java
@@ -4,6 +4,12 @@ public class emplyeewagebuilder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation");
+		int IS_FULL_TIME = 1;
+	      double empCheck = Math.floor(Math.random() * 10) % 2;
+	      if (empCheck == IS_FULL_TIME)
+	         System.out.println("Employee is Present");
+	      else
+	         System.out.println("Employee is Absent");
 	}
 
 }
