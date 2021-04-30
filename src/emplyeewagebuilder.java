5991aa8b98f5fae307db89206e359457a44b2bc9
sanketb412 <sanketbagde38@gmail.com>
2021-04-30 09:10:48 +0530

    Added code using class and method

--- src/emplyeewagebuilder.java
+++ src/emplyeewagebuilder.java
@@ -1,33 +1,45 @@
 
-public class emplyeewagebuilder {
+public class emplyeewagebuilder
+{
+	
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 100;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee Wage Computation");
-		int empHrs=0, totalEmpHrs = 0, totalWorkingDays = 0;
+	
+	
+	public static int EmpWage() 
+	{
+		
+		int empHrs=0;
+		int totalEmpHrs=0;
+		int totalWorkingDays = 0;
+		
 		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 		{
 			totalWorkingDays++;
-		double empCheck = (Math.floor(Math.random() * 10 ) % 3);
-	      int IntValue = (int) Math.round(empCheck);
-	      switch (IntValue)
-	      {
-	      	case IS_PART_TIME: 
-	      		empHrs = 4; break;
-	      	case IS_FULL_TIME:
-	      		empHrs = 8; break;
-	      	default:
-	      		empHrs = 0; break;
-	      }
-	      totalEmpHrs += empHrs;
+			double empCheck = (Math.floor(Math.random() * 10 ) % 3);
+			int IntValue = (int) Math.round(empCheck);
+			switch (IntValue)
+			{
+	      		case IS_PART_TIME: 
+	      			empHrs = 4; break;
+	      		case IS_FULL_TIME:
+	      			empHrs = 8; break;
+	      		default:
+	      			empHrs = 0; break;
+			}
+			totalEmpHrs += empHrs;
 	      
-	      System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
-	      }
-		  int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;	
-		  System.out.println("Total Emp Wage: " + totalEmpWage);
-	}  
+		}
+		totalWorkingDays = (EMP_RATE_PER_HOUR * totalEmpHrs);
+		return totalWorkingDays;
+	}
+	public static void main(String[] args)
+	{
+	
+	System.out.println("Welcome to Employee Wage Computation");
+	System.out.println("Total Emp Salary: " +EmpWage());
+	}
 }
